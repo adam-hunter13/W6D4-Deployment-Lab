186a6f63a208454bfa46b58560d37f693fa761a9
Adam Hunter <hunteradam13@gmail.com>
2021-10-21 15:35:28 -0600

    edit 2

--- server/index.js
+++ server/index.js
@@ -4,11 +4,27 @@ const path = require('path')
 const app = express()
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../deployment.html'));
+  res.sendFile(path.join(__dirname, '../home.html'));
+});
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../about.html'));
+});
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../resume.html'));
+});
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../projects.html'));
 });
 
 app.get('/css',function(req,res) {
     res.sendFile(path.join(__dirname, '../styles.css'));
+});
+
+app.get('/js',function(req,res) {
+    res.sendFile(path.join(__dirname, '../script.js'));
 });
 
 const port = process.env.PORT || 4567
