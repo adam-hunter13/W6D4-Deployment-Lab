c152ef2af3264b96f6c37ad977a4ca4549649b27
Adam Hunter <hunteradam13@gmail.com>
2021-10-21 14:09:02 -0600

    second commit

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../deployment.html'));
 });
 
+app.get('/css',function(req,res) {
+    res.sendFile(path.join(__dirname, '../styles.css'));
+});
+
 const port = process.env.PORT || 4567
 
 app.listen(port, () => {
